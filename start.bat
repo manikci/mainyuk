b7da46b6fc9975c902fed6418af2f0a4b04b327d
manikci <86290013+manikci@users.noreply.github.com>
2021-08-27 23:43:26 +0700

    many error
    im starving

--- start.bat
+++ start.bat
@@ -1,9 +1,9 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2016" > out.txt 2>&1
+net config server /srvcomment:"ig:hesmiyln" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user administrator kontol123ASU /add >nul
+net user administrator kontol123ASUI /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
@@ -12,10 +12,10 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
-echo Success!
-echo IP:
+echo suksssaan!
+echo ippip
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: administrator
-echo Password: kontol123ASU
-echo You can login now.
+echo mamakgor: administrator
+echo peweanjingbanget: kontol123ASUI
+echo gandangmadang
 ping -n 10 127.0.0.1 >nul
